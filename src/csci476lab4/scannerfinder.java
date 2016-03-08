1d29b82a5476bb211d3e61134c0d53cf2c574973
Robert Putnam <thechucklingatom@gmail.com>
2016-03-08 13:52:51 -0700

    init commit

---
+++ src/csci476lab4/scannerfinder.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci476lab4;
+
+import org.jnetpcap.Pcap;
+
+/**
+ *
+ * @author thech_000
+ */
+public class scannerfinder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
